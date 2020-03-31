2cf1d6e1b943ba0ecf5f5281e1a2f438ab80e71a
Kazuya Izumi <shikibu9419@gmail.com>
2020-03-31 22:46:09 +0900

    update

--- main.py
+++ main.py
@@ -10,17 +10,22 @@ POCKET_URL = 'https://getpocket.com/v3/get'
 ACCESS_TOKEN = os.environ.get('POCKET_ACCESS_TOKEN')
 CONSUMER_KEY = os.environ.get('POCKET_CONSUMER_KEY')
 
-MAX_ARTICLES = os.environ.get('MAX_ARTICLES') or 1
-PICK_UP_TYPE = os.environ.get('PICK_UP_TYPE') or 'newest'  # newest / random
-AVAILABLE_TAGS = os.environ.get('AVAILABLE_TAGS') or ['todo', 'business', 'programming']
-TAG = os.environ.get('TAG') or random.choice(AVAILABLE_TAGS)
+MAX_ARTICLES = os.environ.get('MAX_ARTICLES') or 2
+PICK_UP_TYPE = os.environ.get('PICK_UP_TYPE') or 'newest'
+TAG = os.environ.get('TAG') or '_untagged_'
 
 
-def get_pocket_items(tag='_untagged_', sort='newest') -> List[Dict]:
-    params = {'access_token': ACCESS_TOKEN, 'consumer_key': CONSUMER_KEY, 'tag': TAG, 'count': MAX_ARTICLES}
+def get_pocket_items() -> List[Dict]:
+    params = {
+        'access_token': ACCESS_TOKEN,
+        'consumer_key': CONSUMER_KEY,
+        'count': MAX_ARTICLES,
+        'sort': PICK_UP_TYPE,
+        'tag': TAG
+    }
     return requests.post(POCKET_URL, params=params).json()['list'].values()
 
 
 if __name__ == '__main__':
-    posts = get_pocket_items(tag='todo')
+    posts = get_pocket_items()
     notify(TAG, PICK_UP_TYPE, posts)
